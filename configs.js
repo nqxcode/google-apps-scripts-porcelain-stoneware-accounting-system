5c3a6788c646bdda6f6af99cf02ea9ebc571f685
Andrey Pokoev <nc101ux@gmail.com>
2021-09-11 01:46:41 +0300

    Fix configs

--- configs.js
+++ configs.js
@@ -1,1 +1,5 @@
+// my 
 let spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1LdpB3l_gwgvOv1RL5t00kOQENtct-Op3wV7B9UzkagE/edit#gid=895656586"
+
+// oleg
+//let spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1Riopm1SZqcP_ny7ChhTMWqKGHCas411gARx-zRO1fuA/edit#gid=563015232"
